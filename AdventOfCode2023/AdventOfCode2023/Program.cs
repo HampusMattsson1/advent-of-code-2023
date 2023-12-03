762d70c44d35e2a1c211b29efc7d0f87509a29fd
Hampus Mattsson <hjm@ownit.nu>
2023-12-03 20:06:49 +0100

    day 3 setup

--- AdventOfCode2023/AdventOfCode2023/Program.cs
+++ AdventOfCode2023/AdventOfCode2023/Program.cs
@@ -1,10 +1,12 @@
 ﻿using AdventOfCode2023;
 using AdventOfCode2023.day1;
 using AdventOfCode2023.day2;
+using AdventOfCode2023.day3;
 
 //Console.WriteLine("Hello, World!");
 
 //await day1.PartOne();
 //await day1.PartTwo();
 //await day2.PartOne();
-await day2.PartTwo();
+//await day2.PartTwo();
+await day3.PartOne();
---
+++ AdventOfCode2023/AdventOfCode2023/day3/main.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2023.day3
+{
+    static internal class day3
+    {
+        public static async Task PartOne()
+        {
+            var input = await Util.GetInput("day3", 1);
+
+            foreach (var line in input)
+            {
+                Console.WriteLine(line);
+            }
+
+            Console.WriteLine("day3 part one result: ");
+        }
+    }
+}
